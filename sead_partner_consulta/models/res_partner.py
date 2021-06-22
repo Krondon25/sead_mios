0c8e503c62b3cdf4da9666a4b9fef22fb62f0a2d
Kristian Rondon <rondonkristian@gmail.com>
2021-06-22 18:09:05 -0400

    [ADD] res_partner_views: Se quito los campos requeridos

--- sead_partner_consulta/models/res_partner.py
+++ sead_partner_consulta/models/res_partner.py
@@ -11,7 +11,11 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
 
-
+    partner_name = fields.Char(string='Nombre Completo',required=False)
+    first_name = fields.Char(required=False)
+    social_reason = fields.Char(string="Razón Social",required=False)
+    tradename = fields.Char(string="Nombre Comercial",required=False)
+    
     def button_consulta(self):
         token ={"token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImNhbmFsZXMudmFyZ2FzLmx1aXNAZ21haWwuY29tIn0.HTNVWLENuwYKS_ndu8zImqstMfnU8ErXDKBupNFyxAo"}
 
@@ -24,65 +28,42 @@ class ResPartner(models.Model):
         if response.status_code == 200:
             data_response =response.content
             data_response = json.loads(data_response)
-            self.name = data_response.get('razonSocial')
-            self.street = data_response.get('direccion')
-            self.phone = data_response.get('telefonos')
-            self.zip = data_response.get('ubigeo')
-            data_distrito = data_response.get('distrito').capitalize()
-            data_provincia = data_response.get('provincia').capitalize()
-            data_departamento = data_response.get('departamento').capitalize()
-            data_departamento = data_response.get('departamento').capitalize()
+            if 'success' not in data_response:
+                self.name = data_response.get('razonSocial')
+                self.tradename = data_response.get('razonSocial')
+                self.social_reason = data_response.get('razonSocial')
+                self.street = data_response.get('direccion')
+                self.phone = data_response.get('telefonos')
+                self.ubigeo = data_response.get('ubigeo')
+                data_distrito = ''
+                data_provincia = ''
+                data_departamento = ''
 
-            distrito = self.env['l10n_pe.res.city.district'].search([])
-            provincia = self.env['res.city'].search([])
-            departamento = self.env['res.country.state'].search([])
+                if data_response.get('distrito'):
+                    data_distrito = data_response.get('distrito').capitalize()
+                if data_response.get('provincia'):
+                    data_provincia = data_response.get('provincia').capitalize()
+                if data_response.get('departamento'):
+                    data_departamento = data_response.get('departamento').capitalize()
 
-            for record_distrito in distrito:
-                if record_distrito.name == data_distrito:
-                    self.l10n_pe_district = record_distrito.id
+                distrito = self.env['l10n_pe.res.city.district'].search([])
+                provincia = self.env['res.city'].search([])
+                departamento = self.env['res.country.state'].search([])
 
-            for record_provincia in provincia:
-                if record_provincia.name == data_provincia:
-                    self.city_id = record_provincia.id
+                for record_distrito in distrito:
+                    if record_distrito.name == data_distrito:
+                        self.l10n_pe_district = record_distrito.id
 
-            for record_departamneto in departamento:
-                if record_departamneto.name == data_departamento:
-                    self.state_id = record_departamneto.id 
-            self.country_id = 173
+                for record_provincia in provincia:
+                    if record_provincia.name == data_provincia:
+                        self.city_id = record_provincia.id
+
+                for record_departamneto in departamento:
+                    if record_departamneto.name == data_departamento:
+                        self.state_id = record_departamneto.id 
+                self.country_id = 173
+            else:
+                mensaje = data_response.get('message')
+                raise ValidationError(mensaje)
         else:
             raise ValidationError("No Se Pudo encontrar la coicidencia, Verifique sus datos")
-
-
-
-
-
-        
-
-        # {
-        #     "ruc":"20131312955",
-        #     "razonSocial":"SUPERINTENDENCIA NACIONAL DE ADUANAS Y DE ADMINISTRACION TRIBUTARIA - SUNAT",
-        #     "nombreComercial":null,
-        #     "telefonos":[],
-        #     "tipo":null,
-        #     "estado":"ACTIVO",
-        #     "condicion":"HABIDO",
-        #     "direccion":"AV. GARCILASO DE LA VEGA NRO. 1472 LIMA LIMA LIMA",
-        #     "departamento":"LIMA",
-        #     "provincia":"LIMA",
-        #     "distrito":"LIMA",
-        #     "fechaInscripcion":null,
-        #     "sistEmsion":null,
-        #     "sistContabilidad":null,
-        #     "actExterior":null,
-        #     "actEconomicas":[],
-        #     "cpPago":[],
-        #     "sistElectronica":[],
-        #     "fechaEmisorFe":null,
-        #     "cpeElectronico":[],
-        #     "fechaPle":null,
-        #     "padrones":[],
-        #     "fechaBaja":null,
-        #     "profesion":null,
-        #     "ubigeo":"150101",
-        #     "capital":"LIMA"
-        # }'
